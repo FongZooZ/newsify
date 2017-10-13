0514eca472da06e71df102d0c7c9e064906597e4
Kim Nam Phong <fongzooz@FongZooZ.local>
2017-10-13 16:11:43 +0700

    first commit

---
+++ core/config/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default {
+	newsAPI: {
+		apikey: 'a10e75e9535d437abf9c90621fdbe3aa',
+		url: 'https://newsapi.org/v1/'
+	},
+	newsSources: ['techcrunch', 'bbc-news', 'bloomberg', 'business-insider', 'buzzfeed', 'cnn', 'espn', 'national-geographic', 'polygon']
+};
