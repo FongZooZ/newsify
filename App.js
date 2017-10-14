d6207f6cb63fc3fc78f4fb47af1be94696e681dc
Kim Nam Phong <fongzooz@FongZooZ.local>
2017-10-14 09:57:19 +0700

    re-organize folder structure

--- App.js
+++ App.js
@@ -2,34 +2,15 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { getNewsFromSource } from './core/lib/network';
-import { newsSources } from './core/config';
+import Home from './app/components/home';
 
 export default class App extends Component {
-  async componentDidMount() {
-    let news = [];
-    for (let source in newsSources) {
-      let articles = await getNewsFromSource(source);
-      news.push(articles);
-    }
-  }
+	componentDidMount() {
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your aha app!</Text>
-        <Text>Changes you make will automatically reload or not.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<Home />
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/components/home.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { getNewsFromSource } from '../../core/lib/network';
+import { newsSources } from '../../core/config';
+
+export default class Home extends Component {
+	async componentDidMount() {
+		let news = [];
+		for (let source in newsSources) {
+			let articles = await getNewsFromSource(source);
+			news.push(articles);
+		}
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your aha app!</Text>
+				<Text>Changes you make will automatically reload or not.</Text>
+				<Text>Shake your phone to open the developer menu.</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
